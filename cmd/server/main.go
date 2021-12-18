978b3da594c9c408a84b42181a0ed4da8808a6c2
Caio Tony <caiotony16@gmail.com>
2021-12-18 20:28:21 +0100

    feat: add godotenv support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	log.Println("Server running ...")
 
-	listener, err := net.Listen("tcp", ":50051")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	log.Println("🔥 Server listening on port " + port)
 
 	server := grpc.NewServer()
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Token_service
 
 go 1.13
 
-require google.golang.org/grpc v1.43.0
+require (
+	github.com/joho/godotenv v1.4.0
+	google.golang.org/grpc v1.43.0
+)
